42222e571f0732057d7012c5a475ddb7aa08fd09
Abishekvarshan <varsityabi@gmail.com>
2026-02-23 19:05:58 +0530

    feat: webserver added

---
+++ webserver.js
@@ -0,0 +1,6 @@
+const http = require('http'); 
+const app=http.createServer(function (req, res) { 
+  res.writeHead(200, {'Content-Type': 'text/html'}); 
+  res.write('Hello World!'); 
+  res.end(); 
+}).listen(8080);
